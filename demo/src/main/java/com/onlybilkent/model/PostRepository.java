9f7d444989eb60ef1914cf73206a06d868be1d5d
Bartu Albayrak <91389509+BartuAlb@users.noreply.github.com>
2023-12-06 15:07:05 +0300

    Update PostRepository.java

--- demo/src/main/java/com/onlybilkent/model/PostRepository.java
+++ demo/src/main/java/com/onlybilkent/model/PostRepository.java
@@ -4,9 +4,13 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, ObjectId>{
-   
+
+    Optional<Post> findPostByTitle(String title);
+
+
 }
