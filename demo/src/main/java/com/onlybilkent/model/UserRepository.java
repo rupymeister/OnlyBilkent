4bab563281ef51c720fd98b48b5e173189a0e222
ipeksnmz <ipek.sonmez@ug.bilkent.edu.tr>
2023-12-09 22:45:34 +0300

    minor Changes

--- demo/src/main/java/com/onlybilkent/model/UserRepository.java
+++ demo/src/main/java/com/onlybilkent/model/UserRepository.java
@@ -1,5 +1,7 @@
 package com.onlybilkent.model;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
